d0d1492d0b3903e96efda632268c0f254879fdf0
Laurent <LaurentValentin@outlook.com>
2026-06-04 19:19:43 +0400

    first working version of ai coding agent complete

--- main.py
+++ main.py
@@ -1,54 +1,94 @@
+import sys
 import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.call_functions import callable_functions
+from google.genai.types import FunctionResponse
+from functions.call_functions import callable_functions, call_function
 
 # set up environment variables
 load_dotenv()
 api_key = os.environ.get("GOOGLE_API_KEY")
 client = genai.Client(api_key=api_key)
 
-# read in system prompt
-with open(file="system_prompt.md", mode="r") as f:
-    system_prompt = f.read()
 
-# create argument parser
-parser = argparse.ArgumentParser(description="Gemini Chatbot")
-parser.add_argument("user_prompt", type=str, help="User Prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+def main():
 
-# set up role tracking
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    # read in system prompt
+    with open(file="system_prompt.md", mode="r") as f:
+        system_prompt = f.read()
 
-# send the request to google
-response = client.models.generate_content(
-    model="gemini-2.5-pro",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        tools=[callable_functions], system_instruction=system_prompt
-    ),
-)
+    # create argument parser
+    parser = argparse.ArgumentParser(description="Gemini Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-metadata_stats = response.usage_metadata.__dict__
+    # set up role tracking
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-if metadata_stats is None:
-    raise RuntimeError("Failed request. Please try again")
+    # initiate the agent loop. 20 maximum iterations
+    for iter_val in range(20):
+        # send the request to google
+        response = client.models.generate_content(
+            model="gemini-2.5-pro",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[callable_functions], system_instruction=system_prompt
+            ),
+        )
 
-prompt_tokens = metadata_stats["prompt_token_count"]
-output_tokens = metadata_stats["candidates_token_count"]
+        # track model responses so far
+        if response.candidates:
+            for prev_response in response.candidates:
+                messages.append(prev_response.content)
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {output_tokens}")
+        # collect stats (token counts) for logging to console
+        metadata_stats = response.usage_metadata.__dict__
 
-# side-effect: logging model response or function calls to console
-if response.function_calls is None:
-    print(f"Response: {response.text}")
+        if metadata_stats is None:
+            raise RuntimeError("Failed request. Please try again")
 
-else:
-    for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+        prompt_tokens = metadata_stats["prompt_token_count"]
+        output_tokens = metadata_stats["candidates_token_count"]
+
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {prompt_tokens}")
+            print(f"Response tokens: {output_tokens}")
+
+        # break condition. If no new responses, exit agent loop
+        if response.function_calls is None:
+            print(f"Response: {response.text}")
+            return
+
+        else:
+            for function_call in response.function_calls:
+                if args.verbose:
+                    result = call_function(function_call, verbose=True)
+
+                    if not bool(result.parts):
+                        raise Exception("Function call does not contain a .parts attribute")
+
+                    elif type(result.parts[0].function_response) is not FunctionResponse:
+                        raise Exception("Function call did not return a FunctionResponse object")
+
+                    elif result.parts[0].function_response.response is None:
+                        raise Exception("Function response is empty")
+
+                    if args.verbose:
+                        print(f"-> ({result.parts[0].function_response.response})")
+
+        # append function call results to messages list
+        messages.append(result)
+
+        # as a final check, if we've reached the max num of iterations and there's
+        # still no response, exit with code 1
+        if iter_val == 20:
+            print("\nMaximum number of iterations reached, exiting with sys code 1")
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
--- system_prompt.md
+++ system_prompt.md
@@ -7,4 +7,5 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Run (Execute) Python files with optional arguments
 - Write or overwrite files
 
+Pay particular attention to keywords. It's highly likely that when the user asks you to "run" or "execute" a file, you may want to consider using the run_python_file function. For example, "run tests.py" means execute the Python file tests.py. Do not confuse this with get_files_info.
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
